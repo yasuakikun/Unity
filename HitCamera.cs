ffb3fb3f114ab76f31f4142a9270e450731e4430
yasuakikun <jokerbiohazard123@docomo.ne.jp>
2018-06-25 20:00:46 +0900

    unity

---
+++ HitCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCamera : MonoBehaviour {
+
+    private YureCamera yureCamera;
+	// Use this for initialization
+	void Start ()
+    {
+        yureCamera = GetComponent<YureCamera>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+    public void OnTriggerEnter(Collider xhit)
+    {
+        switch (xhit.gameObject.tag)
+        {
+            case "EnemyHit":
+                //yureCamera.Shake(gameObject);
+                break;
+            case "WolfHit":
+                //yureCamera.Shake(gameObject);
+                break;
+            case "HobgoHit":
+                //yureCamera.Shake(gameObject);
+                break;
+            case "goblinHit":
+                //yureCamera.Shake(gameObject);
+                break;
+            case "TrollHit":
+                //yureCamera.Shake(gameObject);
+                break;
+        }
+    }
+}
---
+++ YureCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YureCamera : MonoBehaviour
+{
+    [SerializeField] GameObject[] shakeObjects;
+    [SerializeField] float ShakeTime;
+    [SerializeField] float ShakeDur;
+    private CharacterController cCon;
+    private void Start()
+    {
+        cCon = GetComponent<CharacterController>();
+    }
+
+    public void OnTriggerStay(Collider xhit)
+    {
+        switch (xhit.gameObject.tag)
+        {
+            case "EnemyHit":
+                Shake(shakeObjects);
+
+                break;
+            case "WolfHit":
+                Shake(shakeObjects);
+                break;
+            case "HobgoHit":
+                Shake(shakeObjects);
+                break;
+            case "goblinHit":
+                Shake(shakeObjects);
+                break;
+            case "TrollHit":
+                Shake(shakeObjects);
+                break;
+        }
+    }
+
+    public void Shake(GameObject[] shakeObjects)
+    {
+        foreach(var shakeObject in shakeObjects)
+        {
+            iTween.ShakePosition(shakeObject, Vector3.one * ShakeTime, ShakeDur);
+
+            iTween.PunchPosition(shakeObject, iTween.Hash("x", 0.2f, "y", 0.1f, "time", 0.1f));
+        }
+    }
+}
